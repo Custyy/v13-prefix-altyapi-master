cf47b11214f42226e5e0eabb7af787e6f75e54b7
Clarence <69308633+custyTR@users.noreply.github.com>
2022-11-19 02:52:52 +0300

    Update Eval.js

--- commands/Eval.js
+++ commands/Eval.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
 
-  if (!config.bot.sahip.includes(message.author.id)) return
+  if (!config.bot.owners.includes(message.author.id)) return
   try {
     let codein = args.join(" ")
     let code = eval(codein)
